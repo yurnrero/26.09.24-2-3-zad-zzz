35ec85639386742a16c2ce7a091c514b92e67927
eltsovya <eltsovya@310D-10>
2024-09-26 11:33:40 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _26._09._24_2_3_zad_zzz
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            t1.TextChanged += (s, e) =>
+            {
+                if (double.TryParse(t1.Text, out double value))
+                {
+                    slider1.Value = value;
+                }
+            };
+
+            slider1.ValueChanged += (s, e) =>
+            {
+                t1.Text = slider1.Value.ToString();
+            };
+
+            fontSizeInput.TextChanged += (s, e) =>
+            {
+                if (double.TryParse(fontSizeInput.Text, out double fontSize))
+                {
+                    textBlock.FontSize = fontSize;
+                }
+            };
+
+            fontSizeSlider.ValueChanged += (s, e) =>
+            {
+                textBlock.FontSize = fontSizeSlider.Value;
+            };
+        }
+    }
+
+        
+
+    
+
+
+}
